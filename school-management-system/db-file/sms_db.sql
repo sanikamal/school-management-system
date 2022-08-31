9f487df86602a7b8906be3734df5e53d54cf1e14
ceonmintoh <nmintoh@gmail.com>
2022-08-31 17:16:18 +0100

    edited db file

--- school-management-system/db-file/sms_db.sql
+++ school-management-system/db-file/sms_db.sql
@@ -8,7 +8,7 @@
 -- PHP Version: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET time_zone = "+01:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
